09d297e61fedf9f96f31520236df4782fe946a13
Jonathan Warner <jaxbot@gmail.com>
2015-05-13 22:33:52 -0400

    Clean up

--- index.js
+++ index.js
@@ -3,19 +3,10 @@ var fs = require('fs');
 var React = require('react');
 var App = require('./app/App.jsx');
 
-
-var text = "This is a blog post";
-var text2 = "This is a differnet blog post";
-
-fs.writeFileSync("test.html", React.renderToString(App({ post: text })));
-fs.writeFileSync("test2.html", React.renderToString(App({ post: text2 })));
-fs.writeFileSync("test.html.raw", text);
-fs.writeFileSync("test2.html.raw", text2);
-
 var config = {};
 
 if (process.argv[2] == "devel")
-	config.base = config.baseDevel;
+  config.base = config.baseDevel;
 
 var marked = require("marked");
 var YAML = require("yamljs");
@@ -25,28 +16,26 @@ var sitemapdata = "";
 var rssdata = "";
 
 fs.readdir('input/', function(err, files){
-	for (var i=0; i < files.length; i++) {
-		var filename = files[i].substring(0, files[i].length - 3);
-		var insides = fs.readFileSync('input/' + files[i]).toString();
+  for (var i=0; i < files.length; i++) {
+    var filename = files[i].substring(0, files[i].length - 3);
+    var insides = fs.readFileSync('input/' + files[i]).toString();
 
-		var scope = Object.create(config);
+    var scope = Object.create(config);
 
-		scope.insides = marked(insides.replace(/---([\s\S]+)---/g, ""));
+    scope.insides = marked(insides.replace(/---([\s\S]+)---/g, ""));
 
-		if (filename !== "index") {
-			var frontMatter = YAML.parse(/---([\s\S]+)---/g.exec(insides)[1]);
-			scope.title = frontMatter.title;
-			scope.description = frontMatter.description || scope.description;
-			scope.date = frontMatter.date;
-            scope.post = scope.insides;//postfile;//varFill(postfile, scope);
-		}
+    if (filename !== "index") {
+      var frontMatter = YAML.parse(/---([\s\S]+)---/g.exec(insides)[1]);
+      scope.title = frontMatter.title;
+      scope.description = frontMatter.description || scope.description;
+      scope.date = frontMatter.date;
+    }
+    scope.post = scope.insides;
 
-		scope.contents = React.renderToString(App(scope));
+    scope.contents = React.renderToString(App(scope));
 
-		fs.writeFileSync('public/' + filename + ".html", scope.contents);
-		fs.writeFileSync('public/' + filename + ".html.raw", scope.post);
+    fs.writeFileSync('public/' + filename + ".html", scope.contents);
+    fs.writeFileSync('public/' + filename + ".html.raw", scope.post);
 
-		var img = "";
-	}
 });
 
